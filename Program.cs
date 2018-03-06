db350c1dd73df4c28f19ab521a8c7a1c0ce5c22a
thoong <dkim2442@gmail.com>
2018-03-06 17:28:01 +0700

    BT tuan 1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void LayIP()
+        {
+            String strHostName = Dns.GetHostName();
+            Console.WriteLine("Host Name: " + strHostName);
+            IPHostEntry iphostentry = Dns.GetHostByName(strHostName);
+            for (int i = 0; i < iphostentry.AddressList.Length; i++)
+            {
+                Console.WriteLine(iphostentry.AddressList[i]);
+            }
+        }
+        static void nslookupip(string a)
+        {
+            IPHostEntry entry = Dns.Resolve(a);
+            Console.WriteLine(entry.HostName);
+            Console.WriteLine(entry.AddressList[0]);
+        }
+        
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
